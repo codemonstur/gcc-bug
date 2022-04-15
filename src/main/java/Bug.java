4ce16dfaf17b2ba1fb4b3e4bb3e36fc4c8f61dd0
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2022-04-15 12:06:03 +0200

    Initial commit

---
+++ src/main/java/Bug.java
@@ -0,0 +1,12 @@
+import com.googlecode.htmlcompressor.compressor.ClosureJavaScriptCompressor;
+
+import static com.google.javascript.jscomp.CompilationLevel.ADVANCED_OPTIMIZATIONS;
+
+public class Bug {
+
+    public static void main(final String... args) {
+        final var compress = new ClosureJavaScriptCompressor(ADVANCED_OPTIMIZATIONS);
+        compress.compress("boe");
+    }
+
+}
