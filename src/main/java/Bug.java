1e9c871e4d55269f624aeaf931b33488f0719e3e
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2022-05-22 14:50:34 +0200

    Updated versions

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -1,5 +1,9 @@
 import com.googlecode.htmlcompressor.compressor.ClosureJavaScriptCompressor;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.function.Supplier;
+
 import static com.google.javascript.jscomp.CompilationLevel.ADVANCED_OPTIMIZATIONS;
 
 public class Bug {
@@ -7,6 +11,13 @@ public class Bug {
     public static void main(final String... args) {
         final var compress = new ClosureJavaScriptCompressor(ADVANCED_OPTIMIZATIONS);
         compress.compress("boe");
+
+        final Object[] objects = fillArray(new Object[10], Object::new);
+    }
+
+    public static <T> T[] fillArray(final T[] array, final Supplier<T> generator) {
+        for (int i = 0; i < array.length; i++) array[i] = generator.get();
+        return array;
     }
 
 }
